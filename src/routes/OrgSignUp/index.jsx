fbe81edc5f05179f66ca171df58dd9125989effb
johnswaroop <71548636+johnswaroop@users.noreply.github.com>
2021-10-30 18:23:10 +0530

    major update org signup ui

--- src/routes/OrgSignUp/index.jsx
+++ src/routes/OrgSignUp/index.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './orgSignUp.scss'
 import step1img from '../../images/nftHero.png'
 function OrgSignUp() {
+
+    const [currentStep, setCurrentStep] = useState(0);
+
+    const steps =
+        [
+            <Step1 setCurrentStep={setCurrentStep} />,
+            <Step2 setCurrentStep={setCurrentStep} />,
+            <Step3 setCurrentStep={setCurrentStep} />
+        ]
+
     return (
         <div className={"signUpPage"}>
             <div className={"signUpCon"}>
                 <div className="signUpProgress">
                     <h1 className="progTitle">Welcome</h1>
                     <p className="progSub">
-                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus fuga nisi eveniet sint atque nulla ratione nemo commodi unde error!
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus fuga nisi eveniet sint atque nulla ratione nemo commodi unde error!
                     </p>
                     <div className="imgSec">
                         <img src={step1img} alt="" />
@@ -20,14 +30,14 @@ function OrgSignUp() {
                     </div>
                 </div>
                 <div className="signUpSteps">
-                    <Step2 />
+                    {steps[currentStep]}
                 </div>
             </div>
         </div>
     )
 }
 
-function Step1() {
+function Step1({ setCurrentStep }) {
     return (
         <div className="steps">
             <span className={"inputCon"}>
@@ -39,14 +49,16 @@ function Step1() {
                 <input className={"input"} type="text" />
             </span>
             <span className="navBtns">
-                <button className={"btn"}>Continue</button>
+                <button
+                    className={"btn"}
+                    onClick={() => { setCurrentStep(currentStep => currentStep + 1) }}>Continue</button>
             </span>
         </div>
     )
 }
 
 
-function Step2() {
+function Step2({ setCurrentStep }) {
     return (
         <div className="steps">
             <span className={"inputCon"}>
@@ -58,13 +70,15 @@ function Step2() {
                 <textarea className={"inputTextarea"} type="text" rows="10" />
             </span>
             <span className="navBtns">
-                <button className={"btn"}>Continue</button>
+                <button
+                    className={"btn"}
+                    onClick={() => { setCurrentStep(currentStep => currentStep + 1) }}>Continue</button>
             </span>
         </div>
     )
 }
 
-function Step3() {
+function Step3({ setCurrentStep }) {
     return (
         <div className="steps">
             <span className={"inputCon"}>
